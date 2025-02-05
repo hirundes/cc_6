6e1d5214b332a1ad83fbaeebada6a550a376f564
HIRUN <hirundesilva97@gmail.com>
2025-02-05 18:32:47 -0500

    CHANGE: Task 1 - Business Profit Calculation - Small change to line 4.

--- cc_6.js
+++ cc_6.js
@@ -1,8 +1,7 @@
 //Task 1 - Business Profit Calculation
     //function calculateProfit 
 function calculateProfit(costPrice, sellingPrice, unitsSold) {
-    let profit = (sellingPrice - costPrice) * unitsSold; //formula for calculating profit
-    return profit;
+    return (sellingPrice - costPrice) * unitsSold; //formula for calculating profit
 }
 let profit = calculateProfit(20, 30, 100) //costPrice, sellingPrice, and unitsSold numbers
     //logged "Task 1" and total profit
