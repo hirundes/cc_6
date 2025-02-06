ebb75695b4c8639fe95c81d24d4ba42689e3ec52
HIRUN <hirundesilva97@gmail.com>
2025-02-05 19:15:15 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -18,3 +18,22 @@ let salesTax = calculateSalesTax(100, 0.07); //amount and taxRate
 console.log("Task 2");
 console.log("   Sale Tax: $", salesTax);
 
+//Task 3 - Employee Bonus Calculation
+    //arrow function for calculateBonus
+const calculateBonus = (salary, performanceRating) => {
+    if (performanceRating = "Excellent") {      //Excellent = Bonus: 20%  of salary returned
+        return 0.20 * salary
+    }
+    if (performanceRating = "Good") {           //Good = Bonus: 10% of salary returned
+        return 0.10 * salary 
+    }
+    if (performanceRating = "Average")          //Average = Bonus: 5% of salary returned
+        return 0.05 * salary
+};
+let bonus = calculateBonus(5000, "Good"); //defining bonus with salary and performance rating
+//Logged "Task 3" and Bonus
+console.log("Task 3");
+console.log("   Bonus: $", bonus);
+
+
+
